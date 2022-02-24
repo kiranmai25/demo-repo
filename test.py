673c7bb13bc51ef52861eb1c56ec1fc49f94bc37
kiranmai25 <99332251+kiranmai25@users.noreply.github.com>
2022-02-24 18:54:17 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+import app
+
+class TesterClass(unittest.TestCase):
+
+    def test_rev_word(self):
+        self.assertEqual(app.rev_word("this is a unit test"),"test unit a is this")
+
+if __name__ == '__main__':
+    unittest.main()
