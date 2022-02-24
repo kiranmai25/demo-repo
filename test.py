798f2d8265cce1a474996d31afcfcde43132fa84
kiranmai25 <99332251+kiranmai25@users.noreply.github.com>
2022-02-24 19:03:01 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import app
-
-class TesterClass(unittest.TestCase):
-
-    def test_rev_word(self):
-        self.assertEqual(app.rev_word("this is a unit test"),"test unit a is this")
-
