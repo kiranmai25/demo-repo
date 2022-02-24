7909ec4627818e5f582570914f6913ba868dbf93
kiranmai25 <99332251+kiranmai25@users.noreply.github.com>
2022-02-24 18:55:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,5 +6,3 @@ class TesterClass(unittest.TestCase):
     def test_rev_word(self):
         self.assertEqual(app.rev_word("this is a unit test"),"test unit a is this")
 
-if __name__ == '__main__':
-    unittest.main()
